d78bed82016c479ac4927639f8d3477c2eae072b
Safidi <safidiniainaromino13@gmail.com>
2023-04-01 08:01:46 +0300

    feat: add fonts

---
+++ components/Sections/Home/Banner.vue
@@ -0,0 +1,12 @@
+<template>
+    <section id="banner">
+        <div class="banner__wrapper">
+            <div class="banner__content">
+                
+            </div>
+            <div class="banner__bg">
+
+            </div>
+        </div>
+    </section>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -9,8 +10,37 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily:{
+          sans:['Graphik-regular','sans-serif', ...defaultTheme.fontFamily.sans],
+          'bold':['Graphik-bold','sans-serif'],
+          'light':['Graphik-light', 'sans-serif']
+        }
+    },
+    },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1210px',
+          },
+          '@screen xl': {
+            maxWidth: '1210px',
+          },
+        },
+        '.header__nav-network':{
+          maxWidth: '170px'
+        }
+      })
+    }
+  ],
 }
 
